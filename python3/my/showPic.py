74867cf637a659df2eb0570f33b201125cca27c7
liugaoyong_88@163.com <liugaoyong_88@163.com>
2018-06-30 23:08:49 +0800

    add phyCal.py

--- python3/my/showPic.py
+++ python3/my/showPic.py
@@ -1,20 +1,25 @@
 #!/bin/env python
+# -*- coding:utf-8 -*-
 #------------------------------------------------------------------------------
 # Author: LiuGaoyong                                                          |
 # E-mail: liugaoyong_88@163.com, liugaoyong88@gmail.com                       |
 # Description:                                                                 
 #                            
                              
-import matplotlib.pyplot as plt # plt 用于显示图片
-import matplotlib.image as mpimg # mpimg 用于读取图片
+import matplotlib.pyplot as plt 
+import matplotlib.image as mpimg 
 import numpy as np
 
 #png=read()
 
 
-lena = mpimg.imread('lena.png') # 读取和代码处于同一目录下的 lena.png 此时 lena 就已经是一个 np.array 了，可以对它进行任意处理
+lena = mpimg.imread(r'/mnt/c/User/LiuGaoyong/Pictures/o9.jpg') 
 lena.shape #(512, 512, 3)
 
 plt.imshow(lena) # 显示图片
 plt.axis('off') # 不显示坐标轴
 plt.show()
+
+
+
+
