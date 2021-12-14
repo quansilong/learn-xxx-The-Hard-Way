1f5eaf531eae2fd1301610170682f87a238db300
Liu Gaoyong <liugaoyong_88@163.com>
2021-12-14 16:04:07 +0800

    update contour

--- 2021-11-24_how_to_explore_PES/contour_explorer/first-test.py
+++ 2021-11-24_how_to_explore_PES/contour_explorer/first-test.py
@@ -4,16 +4,19 @@
 # source code:  https://wiki.fysik.dtu.dk/ase/_modules/ase/md/contour_exploration.html#ContourExploration
 
 from ase.md.contour_exploration import ContourExploration as Contour
-from ase.build import molecule
+from ase.build import bulk, molecule
 from ase.calculators.emt import EMT
 
 
-atoms = molecule("CH3CH2OH")
+#atoms = molecule("CH3CH2OH")
+atoms = bulk("Pd" , "fcc", 3.5, cubic=True)
+atoms *= (2,2,2)
+atoms.pbc = True
 atoms.calc = EMT()
 test = Contour(atoms,
     maxstep=0.5,        # 每次迭代中，原子的最大可移动距离
     parallel_drift=0.1, # 每次迭代中，与等高线平行但是方向随机的部分【用于打破对称性】
-    energy_target=None, # 恒势能器尝试保持的系统目标势能，默认是初始构型的势能
+    energy_target=35., # 恒势能器尝试保持的系统目标势能，默认是初始构型的势能
     angle_limit=20,     # 使用曲率改变方向的最大角度
                         #       通常小于30度就可以给出合理的结果，
                         #       且角度越小恒势能的精度也越高，默认是20度
@@ -21,7 +24,7 @@ test = Contour(atoms,
                         #       恒势能步长是由当前势能、目标势能和当前受力
                         #       线性外推得到的。该系数大于1.0会过矫正，而小于1.0会欠矫正。
                         #       默认情况下，会根据parallel_drift的值来调整这个数值。
-    remove_translation=False,       #
+    #remove_translation=True,       #
     logfile='-',
     trajectory="a.traj"
     )
